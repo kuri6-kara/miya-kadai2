4fda8fe80ca9768a6c777c9140994303ca9dd932
miya <tukiya8@yahoo.co.jp>
2025-05-06 21:54:54 +0900

    商品登録画面

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -51,9 +51,9 @@
                 </div>
                 <div class="form__group-content">
                     <div class="form__input--file">
-                        <img src="{{ '/storage/' . $contact['image_file'] }}">
+                        <img src="{{ '/storage/' . $product['image'] }}">
                         <input type="file" name="image">
-                        <input type="hidden" name="image" value="{{ $products['image']}}">
+                        <input type="hidden" name="image" value="{{ $product['image']}}">
                     </div>
                     <div class="form__error">
                         @error('image')
