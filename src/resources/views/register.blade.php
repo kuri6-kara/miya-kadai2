e5d05474a40421cf011395e0608c0084eef9578c
miya <tukiya8@yahoo.co.jp>
2025-05-06 20:22:39 +0900

    商品登録画面

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -5,5 +5,114 @@
 @endsection
 
 @section('content')
+<div class="contact-form__content">
+    <div class="contact-form__heading">
+        <h2>Contact</h2>
+    </div>
+    <form action="/products/register" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">商品名</span>
+                <span class="form__label--required">必須</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="name" placeholder="商品名を入力" value="{{ old('name') }}" />
+                </div>
+                <div class="form__error">
+                    @error('name')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
 
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">値段</span>
+                    <span class="form__label--required">必須</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="text" name="price" placeholder="値段を入力" value="{{ old('price') }}" />
+                    </div>
+                    <div class="form__error">
+                        @error('price')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">商品画像</span>
+                    <span class="form__label--required">必須</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--file">
+                        <input type="file" name="image">
+                    </div>
+                    <div class="form__error">
+                        @error('image')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">季節</span>
+                    <span class="form__label--required">必須</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            @foreach($seasons as $season)
+                            <label>
+                                <input type="checkbox" name="seasons_ids[]" value="{{ $season->id }}" />
+                                {{ $season->content }}
+                            </label>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="form__error">
+                        <div class="seasons">
+                            @error('gender')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">商品説明</span>
+                    <span class="form__label--required">必須</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--textarea">
+                        <textarea name="description" placeholder="商品の説明を入力" value="{{ old('description') }}">{{ old('description') }}</textarea>
+                    </div>
+                </div>
+                <div class="form__error">
+                    <div class="description">
+                        @error('description')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">戻る</button>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">登録</button>
+        </div>
+    </form>
+</div>
 @endsection
