35302005b757952afe5437aa6073a11bd9802488
miya <tukiya8@yahoo.co.jp>
2025-05-11 03:31:37 +0900

    商品詳細、更新

--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -5,5 +5,13 @@
 @endsection
 
 @section('content')
+<div class="show">
+    <h1>商品名>{{ $product->name }}</h1>
+    <img src="{{ asset('/storage/' . $product->images) }}" alt="{{ $product->name }}">
+
+    <form action="/products/{productId}/update" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+</div>
 
 @endsection
