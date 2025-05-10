b1883d24c79fbb4ff315fb3a16eaf9fd944e486a
miya <tukiya8@yahoo.co.jp>
2025-05-11 03:20:45 +0900

    商品一覧画面

--- src/resources/views/product.blade.php
+++ src/resources/views/product.blade.php
@@ -5,14 +5,14 @@
 @endsection
 
 @section('content')
-<div class="admin">
-    <h2 class="admin__heading content__heading">商品一覧</h2>
+<div class="product">
+    <h2 class="product__heading content__heading">商品一覧</h2>
     <div class="form__button">
         <a class="header__button" href="/products/register">
             +商品を追加
         </a>
     </div>
-    <div class="admin__inner">
+    <div class="product__inner">
         <form class="search-form" action="/products/search" method="post">
             @csrf
             <input class="search-form__keyword-input" type="text" name="keyword" value="{{ request('keyword')}}" placeholder="商品名で検索">
@@ -23,11 +23,17 @@
 
         <div class="card">
             @foreach($products as $product)
-            <div class="card-body">
-                <p class="card-text">{{ $product->name }}</p>
-                <p class="card-text">{{ $product->price }}</p>
-                <a href="#" class="btn btn-primary">詳細</a>
-            </div>
+            <a href="{{ route('products.show', $product->id) }}" class="text-decoration-none text-dark">
+                <div class="card">
+                    <img src="{{ asset('/storage/' . $product->image) }}" alt="{{ $product->name }}">
+                    <div class="card-body">
+                        <div class="card-body">
+                            <p class="card-text">{{ $product->name }}</p>
+                            <p class="card-text">{{ $product->price }}</p>
+                        </div>
+                    </div>
+                </div>
+                </a>
             @endforeach
         </div>
     </div>
