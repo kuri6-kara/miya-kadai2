5a85f98753864ccc28f3e0bce68a54684d5782eb
miya <tukiya8@yahoo.co.jp>
2025-05-11 23:41:31 +0900

    ダミーデータ

--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -48,9 +48,9 @@
         <div class="form__label--item">季節</div>
         <div class="form__input--text">
             @foreach($seasons as $season)
-            <input type="checkbox" name="season_ids[]" value="{{ $season->id }}" />
+            <input type="checkbox" name="season_ids[]" value="{{ $season->id }}">
             {{ $season->content }}
-            <input type="hidden" name="season_ids[]" value="{{ $season->id }}" />
+            <input type="hidden" name="season_ids[]" value="{{ $season->id }}">
             @foreach
         </div>
         <div class="form__error">
