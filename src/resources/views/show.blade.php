fba6ddae915a545612d7dc6e77ab91348c21a51a
miya <tukiya8@yahoo.co.jp>
2025-05-11 13:52:37 +0900

    商品詳細、更新

--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -6,28 +6,47 @@
 
 @section('content')
 <div class="show">
-    <div class="form__label--item">
-        商品名
-    </div>
-    <div class=form__inout--text>
-        <input type="text" name="name" value="{{ $product['name'] }}">
-    </div>
-
-    <div class="form__label--item">値段</div>
-    <div class="form__input--text">
-        <input type="text" name="price" value="{{ $product['price'] }}">
-    </div>
-
-    <div class="form__label--item">季節</div>
-    <div class="form__input--text">
-        <input type="text" name="price" value="{{ $product['price'] }}">
-    </div>
-
-    <img src="{{ asset('/storage/' . $product->images) }}" alt="{{ $product->name }}">
-
     <form action="/products/{productId}/update" method="POST" enctype="multipart/form-data">
-        @csrf
         @method('PUT')
+        @csrf
+
+        <div class="form__label--item">
+            商品画像
+        </div>
+        <div class="form__input--file">
+            <img src="{{ asset('/storage/' . $product->image) }}" alt="{{ $product->name }}">
+            <input type="file" name="image">
+            <input type="hidden" name="image" value="{{ $product['image']}}">
+        </div>
+
+        <div class="form__label--item">
+            商品名
+        </div>
+        <div class=form__inout--text>
+            <input type="text" name="name" value="{{ $product['name'] }}">
+        </div>
+
+        <div class="form__label--item">値段</div>
+        <div class="form__input--text">
+            <input type="text" name="price" value="{{ $product['price'] }}">
+        </div>
+
+        <div class="form__label--item">季節</div>
+        <div class="form__input--text">
+            @foreach($seasons as $season)
+            <input type="checkbox" name="season_ids[]" value="{{ $season->id }}" />
+            {{ $season->content }}
+            <input type="hidden" name="season_ids[]" value="{{ $season->id }}" />
+            @foreach
+        </div>
+
+        <div class="form__label--item">商品説明</div>
+        <div class="form__input--text">
+            <input type="text" name="description" value="{{ $product['description'] }}">
+        </div>
+
+        <img src="{{ asset('/storage/' . $product->images) }}" alt="{{ $product->name }}">
+
 </div>
 
 @endsection
